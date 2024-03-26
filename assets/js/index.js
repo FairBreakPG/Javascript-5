c3e6463f1d6456b0dd289201315f8788bcceebc9
Andrés <51636451+FairBreakPG@users.noreply.github.com>
2024-03-25 21:34:00 -0600

    Se agregan mejoras

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,23 @@
 let tareaTexto = document.querySelector("#txtTarea");
 let listadoTareas = document.querySelector("#listaTareas");
 let btnAgregartareas = document.querySelector("#btnAgregarTarea");
+
 let  arrayTareas = [];
+let contador = 0;
 
 btnAgregartareas.addEventListener("click", () => {
     let textoTarea = tareaTexto.value;
     arrayTareas.push(textoTarea);
     tareaTexto.value = "";
+    contador = arrayTareas.length;
     let html = "";
     for (let tarea of arrayTareas) {
-        html += `<li>${tarea}</li>`;
+        html += `<li>${tarea}
+        <input  id="taraeRealizada"    type="checkbox"></li>
+        <button type="submit" class="" id="eliminarTarea">Eliminar tarea</button> `;
     }
     listaTareas.innerHTML = html;
+    console.log(arrayTareas.length);
+    let contadorElemento = document.querySelector("#contador");
+    contadorElemento.textContent = contador;
 });
